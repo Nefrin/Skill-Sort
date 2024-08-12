6a1d6980fc1a4ccf2541f1b0189bc2a22a5afbae
Hans <gamer1928@gmail.com>
2024-08-12 20:25:06 +0200

    Implemented File Reading

--- skill_sort.js
+++ skill_sort.js
@@ -1,17 +1,40 @@
 class FileProcessor{
     constructor(){
-        this.file_change_callback = function(e) { console.log("File Change default"); };
+        this.file = new File([], "none");
+        this.file_content = "";
+        this.file_changed_callback = function(e) {
+            console.log("file changed default");
+        };
+
+        let self = this;
+
         this.file_input = document.getElementById("input-file");
-        this.file_input.addEventListener("change", this.select_file, false);
+        this.file_input.addEventListener(
+            "change", 
+            function(e) {
+                self.select_file(self, e);
+            },
+            false
+        );
     }
 
-    select_file(){
-        console.log("File Change");
-        this.file_change_callback();
+    select_file(self, event){
+        self.file = event.target.files[0];
+
+        const fileReader = new FileReader();
+        fileReader.onload = function(e){
+            self.file_content = e.target.result;
+            self.file_changed_callback();
+        }
+        fileReader.readAsText(self.file);
     }
 
     get_file_contents(){
+        return this.file_content;
+    }
 
+    set_file_changed_callback(callback){
+        this.file_changed_callback = callback;
     }
 
 }
@@ -21,6 +44,5 @@ var fileProcessor;
 window.onload = function (e) 
 {
     fileProcessor = new FileProcessor();
-
 
 }
